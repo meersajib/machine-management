d56119cca7ecb92f34eacc282fdb9395e9d01d1e
meersajib <meerhaque@gmail.com>
2021-08-11 02:39:13 +0600

    .env ignored

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -15,6 +15,18 @@ export default function Sidebar(props) {
     <>
       <nav className="md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:flex-row md:flex-nowrap md:overflow-hidden shadow-xl bg-black flex flex-wrap items-center justify-between relative md:w-64 z-10 py-4 px-6">
         <div className="md:flex-col md:items-stretch md:min-h-full md:flex-nowrap px-0 flex flex-wrap items-center justify-between w-full mx-auto">
+          {/* Toggler */}
+          <button
+            className="cursor-pointer text-white opacity-50 md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent"
+            type="button"
+            onClick={() => setCollapseShow("bg-black m-2 py-3 px-6")}
+          >
+            <i className="fas fa-bars"></i>
+          </button>
+          {/* Brand */}
+         
+          {/* User */}
+          {/* Collapse */}
           <div className='md:flex-col md:items-stretch md:min-h-full md:flex-nowrap px-0 flex flex-wrap items-center justify-between w-full mx-auto'>
           <Link href='/'>
             <a className='md:block text-left md:pb-2 text-white mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-2 px-0'>
@@ -40,7 +52,7 @@ export default function Sidebar(props) {
                 <div className='w-6/12 flex justify-end'>
                   <button
                     type='button'
-                    className='cursor-pointer text-black opacity-50 md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent'
+                    className='cursor-pointer text-white opacity-50 md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent'
                     onClick={() => setCollapseShow('hidden')}>
                     <i className='fas fa-times'></i>
                   </button>
